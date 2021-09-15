06fbfe082ff5a3d46f212412fc1cefc64c9781d8
pedro218 <pedroaf218@gmail.com>
2021-09-14 23:59:39 -0700

    Implemented logout function

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,11 +7,31 @@
       <div class="links">
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link> |
-        <a href="#">Logout</a>
+        <a href="#" @click="logout">Logout</a>
       </div>
     </nav>
   </div>
 </template>
+
+<script>
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'Navbar',
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    
+    const logout = () => {
+      store.dispatch('logout')
+      router.push('/login')
+    }
+
+    return { logout }
+  }
+}
+</script>
 
 <style scoped>
 .nav {
